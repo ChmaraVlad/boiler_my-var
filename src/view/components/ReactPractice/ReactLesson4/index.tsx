ef74db0bbfe6161eecb569f20bfa4c7b3c19f736
Chmara Vlad <chmaravlad5@gmail.com>
2022-02-16 22:02:51 +0200

    Add ReactLesson4  Component

---
+++ src/view/components/ReactPractice/ReactLesson4/index.tsx
@@ -0,0 +1,21 @@
+// Core
+import React, { FC } from 'react';
+
+// Bus
+// import {} from '../../../bus/'
+
+// Styles
+import * as S from './styles';
+
+// Types
+type PropTypes = {
+    /* type props here */
+}
+
+export const ReactLesson4: FC<PropTypes> = () => {
+    return (
+        <S.Container>
+            Component: ReactLesson4
+        </S.Container>
+    );
+};
